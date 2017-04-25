7e411f375df681a055b702c3f8c49f877758f83c
LLeo <lleoem@gmail.com>
2017-04-25 11:15:26 -0400

    added early disabling of GUI elements

--- init.el
+++ init.el
@@ -1,1 +1,7 @@
 ;; FRESH EMACS CONFIG .init.el
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(when (not window-system)
+  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
