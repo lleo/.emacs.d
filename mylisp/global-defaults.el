5e9f3f7bdb911ffe89841a38ad8d1a95b5c09489
LLeo <lleoem@gmail.com>
2017-04-25 14:23:35 -0400

    added global settings

---
+++ mylisp/global-defaults.el
@@ -0,0 +1,29 @@
+;; for go-mode where I use actual tabs for indentation
+(setq default-tab-width 4)
+
+(setq show-trailing-whitespace t)
+;; M-x set-variable show-trailing-whitespace t
+
+(setq x-stretch-cursor t) ;;show tabs with wide cursor
+
+;; automatically insert matching pair for example type "(" get ")" with the
+;; cursor positioned between them. It is even mode-aware so if you are in
+;; a programming mode less-than "<" is not automatically matched, but if
+;; you are in a HTML mode it will. I think it is context aware for languages
+;; that use both less-than "a < b" and matched angle brackets "<ParamType>".
+;; It'll even delete pairs, you have to see it to understand.
+(electric-pair-mode 1)
+
+;;make emacs scroll more "normally" (ie like other gui editors)
+(setq scroll-conservatively 1) ;;scroll one line at a time
+(setq scroll-margin 1)         ;;keep the next line in view
+
+;; Turn on line numbers on left gutter
+(global-linum-mode t)
+(setq linum-format "%3d")
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
+
+(provide 'global-defaults)
---
+++ mylisp/global-key-bindings.el
@@ -0,0 +1,62 @@
+
+(when is-mac
+  ;; give me back my option key
+  ;; mac-option-modifier is an alias for ns-alternate-modifier
+  ;;(setq ns-alternate-modifier (quote none))
+  (setq mac-option-modifier 'none)
+
+  ;;(setq mac-option-modifier 'meta)
+  ;; now taked away one; make the right option key 'meta
+  (setq mac-right-option-modifier 'meta)
+
+  (setq mac-right-command-modifier 'meta)
+
+  (setq mac-command-modifier 'super)
+
+  (setq mac-funciton-modifier 'hyper)
+
+  ;;(setq mac-control-modifier 'control)
+  ;;(setq mac-right-control-modifier 'control)
+
+  )
+
+
+(global-set-key (kbd "C-x p") 'previous-multiframe-window)
+
+(defun my-copy-rectangle (start end)
+   "Copy the region-rectangle instead of `kill-rectangle'."
+   (interactive "r")
+   (setq killed-rectangle (extract-rectangle start end)))
+
+(global-set-key (kbd "C-x r M-w") 'my-copy-rectangle)
+
+
+(defun my-cut-to-register (register start end)
+  "call copy-to-register with a prefix arg"
+  (interactive "cCopy to register: \nr")
+  (copy-to-register register start end t))
+
+(global-set-key (kbd "C-x r x") 'my-cut-to-register)
+
+
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
+(global-set-key (kbd "C-%") 'goto-match-paren)
+
+;; this requires a previous regex search hence 'search-forward-regex
+(global-set-key (kbd "C-_") 'text-scale-decrease)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+;(global-set-key (kbd "M-#") 'isearch-query-replace-regexp)
+(global-set-key (kbd "M-#") 'query-replace-regexp) ;currently C-M-%
+;(global-set-key (kbd "C-c C-s") 'isearch-forward-regexp)
+;(global-set-key (kbd "C-c C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-/") 'isearch-forward-regexp)
+(global-set-key (kbd "C-?") 'isearch-backward-regexp)
+
+(provide 'global-key-bindings)
