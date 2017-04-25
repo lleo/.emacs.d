9cc3577b3fbcfff1a9f37b87d1bac91e90ab20ac
LLeo <lleoem@gmail.com>
2017-04-25 11:28:49 -0400

    get rid of that dame startup message

--- init.el
+++ init.el
@@ -5,3 +5,5 @@
   (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(setq inhibit-startup-message t)
