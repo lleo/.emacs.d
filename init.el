2e0d7ee2e22acf6f9fa5f7669d1094e0394086c6
LLeo <lleoem@gmail.com>
2017-04-25 12:21:23 -0400

    added my-lisp-dir & elpa-dir variable definitions and added them to the load-path

--- init.el
+++ init.el
@@ -15,3 +15,13 @@
 ;; Keep emacs Custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" dotfiles-dir))
 (load custom-file)
+
+;; Set my-lisp-dir variable to .emacs.d/mylisp
+(setq my-lisp-dir (expand-file-name "mylisp" dotfiles-dir))
+
+;; Define 'elpa-dir
+(setq elpa-dir (expand-file-name "elpa" dotfiles-dir))
+
+;; Set up load path
+(add-to-list 'load-path my-lisp-dir)
+(add-to-list 'load-path elpa-dir)
