3b0d14b38d976826f5d2aab005cda466b6197db2
LLeo <lleoem@gmail.com>
2017-05-07 10:14:06 -0400

    cleaned up a little & installed more packages

--- custom.el
+++ custom.el
@@ -6,8 +6,7 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
-	("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
- '(gofmt-command "goimports")
+	("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(js3-auto-indent-p t)
  '(js3-basic-offset 2)
  '(js3-enter-indents-newline t)
@@ -25,7 +24,7 @@
  '(js3-strict-inconsistent-return-warning nil)
  '(package-selected-packages
    (quote
-	(diff-hl nodejs-repl ac-cider cider-hydra cider-spy cider clojure-cheatsheet clojure-snippets clojure-mode rainbow-delimiters parinfer parinfer-mode paredit go-playground go-rename go-guru go-autocomplete go-mode js3-mode auto-complete multiple-cursors linum-off ido-ubiquitous yasnippet magit color-theme-sanityinc-tomorrow expand-region)))
+	(js2-mode vdiff helm minimap diff-hl nodejs-repl ac-cider cider-hydra cider-spy cider clojure-cheatsheet clojure-snippets clojure-mode rainbow-delimiters parinfer parinfer-mode paredit go-playground go-rename go-guru go-autocomplete go-mode js3-mode auto-complete multiple-cursors linum-off ido-ubiquitous yasnippet magit color-theme-sanityinc-tomorrow expand-region)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -33,6 +32,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit default :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo"))))
- '(whitespace-space ((t (:foreground "dark gray"))))
- '(whitespace-tab ((t (:foreground "darkgray")))))
+ '(default ((t (:inherit default :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
