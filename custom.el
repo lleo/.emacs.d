af63ba01663c7e3d39f7f1027d27e33615c8e87a
LLeo <lleoem@gmail.com>
2017-05-09 14:34:20 -0400

    minor customization of js3-mode cause alot diff noise

--- custom.el
+++ custom.el
@@ -6,15 +6,15 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
-	("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+    ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(js3-auto-indent-p t)
  '(js3-basic-offset 2)
  '(js3-enter-indents-newline t)
  '(js3-global-externs
    (mapcar
-	(quote symbol-name)
-	(quote
-	 (JSON ArrayBuffer Int8Array Uint8Array Uint8ClampedArray Int16Array Uint16Array Int32Array Uint32Array Float32Array Float64Array DataView process Buffer setTimeout setImmediate setInterval clearTimeout clearImmediate clearInterval ref unref console module exports require))))
+    (quote symbol-name)
+    (quote
+     (JSON ArrayBuffer Int8Array Uint8Array Uint8ClampedArray Int16Array Uint16Array Int32Array Uint32Array Float32Array Float64Array DataView process Buffer setTimeout setImmediate setInterval clearTimeout clearImmediate clearInterval ref unref console module exports require))))
  '(js3-highlight-level 3)
  '(js3-indent-on-enter-key t)
  '(js3-lazy-dots t)
@@ -22,9 +22,10 @@
  '(js3-mirror-mode t)
  '(js3-mode-escape-quotes nil)
  '(js3-strict-inconsistent-return-warning nil)
+ '(lisp-indent-function (quote common-lisp-indent-function))
  '(package-selected-packages
    (quote
-	(js2-mode vdiff helm minimap diff-hl nodejs-repl ac-cider cider-hydra cider-spy cider clojure-cheatsheet clojure-snippets clojure-mode rainbow-delimiters parinfer parinfer-mode paredit go-playground go-rename go-guru go-autocomplete go-mode js3-mode auto-complete multiple-cursors linum-off ido-ubiquitous yasnippet magit color-theme-sanityinc-tomorrow expand-region)))
+    (smart-tabs-mode js2-mode vdiff helm minimap diff-hl nodejs-repl ac-cider cider-hydra cider-spy cider clojure-cheatsheet clojure-snippets clojure-mode rainbow-delimiters parinfer parinfer-mode paredit go-playground go-rename go-guru go-autocomplete go-mode js3-mode auto-complete multiple-cursors linum-off ido-ubiquitous yasnippet magit color-theme-sanityinc-tomorrow expand-region)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
