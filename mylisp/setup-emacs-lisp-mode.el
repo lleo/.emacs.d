b097524428ad1ab97e26fc5de7dbf366f9a12acc
LLeo <lleoem@gmail.com>
2017-05-09 15:13:42 -0400

    removed big commented out code and added my own hook to turn on 'indent-tabs-mode

--- mylisp/setup-emacs-lisp-mode.el
+++ mylisp/setup-emacs-lisp-mode.el
@@ -1,4 +1,3 @@
-
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 ;;(add-hook 'emacs-lisp-mode-hook 'parinfer-mode)
 ;; removed realtime with (remove-hook 'emacs-lisp-mode-hook 'parinfer-mode)
@@ -8,27 +7,14 @@
 (eval-after-load "paredit.el"
   '(require 'paredit-menu))
 
-;; from https://github.com/DogLooksGood/parinfer-mode README.org
-;; (use-package parinfer
-;; 			 :ensure t
-;; 			 :bind
-;; 			 (("C-," . parinfer-toggle-mode))
-;; 			 :init
-;; 			 (progn
-;; 			   (setq parinfer-extensions
-;; 					 '(defaults ; should be included.
-;; 						;;pretty-parens; different paren styles for different modes.
-;; 						;;evil         ; If you use Evil.
-;; 						;;lispy        ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-;; 						;;paredit      ; Introduce some paredit commands.
-;; 						;;smart-tab    ; C-b & C-f jump positions and smart shift with tab & S-tab.
-;; 						;;smart-yank   ; Yank behavior depend on mode.
-;; 						)))); 
-;; 
-;; ;;    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-;; ;;    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-;; ;;    (add-hook 'scheme-mode-hook #'parinfer-mode)
-;; ;;    (add-hook 'lisp-mode-hook #'parinfer-mode))
+(defun my-emacs-lisp-mode-hook ()
+  (set 'indent-tabs-mode nil))
+(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
+
+;;(add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+;;(add-hook 'common-lisp-mode-hook #'parinfer-mode)
+;;(add-hook 'scheme-mode-hook #'parinfer-mode)
+;;(add-hook 'lisp-mode-hook #'parinfer-mode))
 
 
 (provide 'setup-emacs-lisp-mode)
