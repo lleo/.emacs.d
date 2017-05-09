75c6f874eec95d5bbc63009b5477aec1a516f041
LLeo <lleoem@gmail.com>
2017-05-09 15:03:20 -0400

    I don't use global-highlight-changes-mode. Instead I setup my own hook function and add it to text-mode-hook and prog-mode-hook which apply to most modes. I also start highlight-changes-visibility-initial-state off. I added my own short named function to toggle highlight-changes-visible-mode, cuz well the name is to damn to type and I couldn't figure out a good key chord to map it to. Now it is just "M-x hlc<enter>".

--- mylisp/setup-changes.el
+++ mylisp/setup-changes.el
@@ -2,10 +2,22 @@
 ;;
 
 (defun my-highlight-changes ()
-  (highlight-changes-mode t))
+  (set 'highlight-changes-visibility-initial-state nil)
+  (highlight-changes-mode t)
+  ;;(highlight-changes-visible-mode -1)
+  )
 
 (add-hook 'text-mode-hook 'my-highlight-changes)
 (add-hook 'prog-mode-hook 'my-highlight-changes)
 
+(defun hlc ()
+  "Toggle highlight-changes-visible-mode with a shorter name."
+  (interactive)
+  (if highlight-changes-visible-mode
+      (highlight-changes-visible-mode -1)
+    (highlight-changes-visible-mode t)))
+
+;;(global-highlight-changes-mode t)
 ;;(global-highlight-changes-mode-hook)
+
 (provide 'setup-changes)
