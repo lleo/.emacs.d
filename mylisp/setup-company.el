c4c9f0a3fa1c7142c9ca77bb95802a81f4dd5a89
LLeo <lleoem@gmail.com>
2019-02-26 05:24:57 -0500

    setup keyboard mapped next&previous for company-mode popup ui

--- mylisp/setup-company.el
+++ mylisp/setup-company.el
@@ -1,6 +1,13 @@
 (require 'company-lsp)
 (push 'company-lsp company-backends)
 
+
+;;(with-eval-after-load 'company
+;;    (define-key company-active-map (kbd "C-n") 'company-select-next)
+;;    (define-key company-active-map (kbd "C-p") 'company-select-previous))
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'setup-company)
--- mylisp/setup-lsp.el
+++ mylisp/setup-lsp.el
@@ -3,7 +3,7 @@
 
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 
-(define-key lsp-ui-peek-mode-map (kbd "C-n") 'lsp-ui-peek--select-next)
-(define-key lsp-ui-peek-mode-map (kbd "C-p") 'lsp-ui-peek--select-prev)
+;;(define-key lsp-ui-peek-mode-map (kbd "C-n") 'lsp-ui-peek--select-next)
+;;(define-key lsp-ui-peek-mode-map (kbd "C-p") 'lsp-ui-peek--select-prev)
 
 (provide 'setup-lsp)
